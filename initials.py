855109ccc6aa38c219b02f2b45f4e9d8ddafc157
princeokoannang <2425402345@live.gctu.edu.gh>
2026-06-09 21:36:43 +0000

    Implement initials printing in stylized format
    Added functionality to print initials in stylized block letters.

--- initials.py
+++ initials.py
@@ -1,7 +1,40 @@
 
 def main():
-  pass #code goes here
+ # File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: Prince Oko Annang
+# STUDENT ID:  2425402345
+# Email: 2425402345@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Prince Oko Annang, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+def main():
+    # Print the initials P, O, A in large 12-wide x 10-high block letters composed
+    # of their respective capital letters and periods. Each letter is followed
+    # by a 4-asterisk period block. Three period columns separate each section.
+
+    print()
+    print("...POA")
+    print()
+    print("...PPPPPPPPPP...........OOOOOOOO.............AAAA.......")
+    print("...PP......PPP.........OO......OO...........AAAAAA......")
+    print("...PP......PPP........OO........OO.........AA....AA.....")
+    print("...PP......PPP........OO........OO........AA......AA....")
+    print("...PPPPPPPPPP.........OO........OO........AA......AA....")
+    print("...PPPPPPPPPP.........OO........OO.......AAAAAAAAAAAA...")
+    print("...PP.................OO........OO.......AA........AA...")
+    print("...PP.................OO........OO.......AA........AA...")
+    print("...PP............**...OO......OO....**...AA........AA..**")
+    print("...PP............**.....OOOOOOOO....**...AA........AA..**")
+    print()
 
 
+main()
+
+main()
 
 main()
